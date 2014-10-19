91f1daddb8b92627872ad50e1e0283ead18d9cc1
J <katie-jaye@codegirlz.com>
2014-10-19 15:27:15 +0100

    tidied up service

--- KatieWillowMartin.TodoList.UI.Web/KatieWillowMartin.TodoList.UI.Web/app/tasks/tasksService.js
+++ KatieWillowMartin.TodoList.UI.Web/KatieWillowMartin.TodoList.UI.Web/app/tasks/tasksService.js
@@ -3,18 +3,18 @@
     'app.tasks',
     'angular'
 ],
-   function(module,tasks, $scope) {
-        module.service('tasksService',
-                        function() {
-                        this.tasks = [
-                                    { name: 'task1', complete: 'true' },
-                                    { name: 'task1', complete: 'false' },
-                                    { name: 'task1', complete: 'true' },
-                                    { name: 'task1', complete: 'false' },
-                                    { name: 'task1', complete: 'true' },
-                                    { name: 'task1', complete: 'true' }
-                                   ];
+   function (module) {
+       module.service('tasksService',
+                       function () {
+                           this.tasks = [
+                                       { name: 'task1', complete: 'true' },
+                                       { name: 'task1', complete: 'false' },
+                                       { name: 'task1', complete: 'true' },
+                                       { name: 'task1', complete: 'false' },
+                                       { name: 'task1', complete: 'true' },
+                                       { name: 'task1', complete: 'true' }
+                           ];
 
-                                   }
-                     );
-    });
+                       }
+       );
+   });
