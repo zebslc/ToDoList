2774d75b4d362a36575e6cf90c798075717867c5
J <katie-jaye@codegirlz.com>
2014-10-20 10:12:10 +0100

    adding code for initially checking and marking up tasks and then changing state

--- KatieWillowMartin.TodoList.UI.Web/KatieWillowMartin.TodoList.UI.Web/app/tasks/tasks.js
+++ KatieWillowMartin.TodoList.UI.Web/KatieWillowMartin.TodoList.UI.Web/app/tasks/tasks.js
@@ -12,9 +12,15 @@
         function tasks($scope, tasksService) {
 
             $scope.tasks = tasksService.tasks;
+            
 
             $scope.delete = function (idx) {
                 tasksService.tasks.splice(idx, 1);
+            };
+
+            $scope.complete=function(idx) {
+                var oldStatus = $scope.tasks[idx].complete;
+                $scope.tasks[idx].complete = !oldStatus;
             };
         }
     });
--- KatieWillowMartin.TodoList.UI.Web/KatieWillowMartin.TodoList.UI.Web/app/tasks/tasksService.js
+++ KatieWillowMartin.TodoList.UI.Web/KatieWillowMartin.TodoList.UI.Web/app/tasks/tasksService.js
@@ -8,12 +8,12 @@
         module.service('tasksService',
             function () {
                 this.tasks = [
-                    { name: 'task1', complete: 'true' },
-                    { name: 'task1', complete: 'false' },
-                    { name: 'task1', complete: 'true' },
-                    { name: 'task1', complete: 'false' },
-                    { name: 'task1', complete: 'true' },
-                    { name: 'task1', complete: 'true' }
+                    { name: 'task1', complete: true },
+                    { name: 'task1', complete: false },
+                    { name: 'task1', complete: true },
+                    { name: 'task1', complete: false },
+                    { name: 'task1', complete: true },
+                    { name: 'task1', complete: true }
                 ];
 
             }
